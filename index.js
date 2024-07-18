60cb23492b065bc0f764575a57e38baa4d47b156
renraelreve <renraelreve@gmail.com>
2024-07-18 22:16:01 +0800

    done node js file

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 require("dotenv").config();
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
 // Middleware to parse JSON request bodies
 app.use(express.json());
 
-const print = require("./controller");
+const controller = require("./controller");
 
 // Route definitions
 app.get('/', controller.print);
@@ -13,5 +13,5 @@ app.get('/home', controller.home);
 app.post('/user', controller.createUser);
 
 app.listen(process.env.PORT, ()=>{
-    console.log(`Listening to port ${process.env.PORT}`)
-})
+    console.log(`Listening to port ${process.env.PORT}`);
+});
